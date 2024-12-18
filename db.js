7dd5ceeb81f58d13bc8a1e11c218785d6d80a0c8
Ajith <134156010+Ajith-ajay@users.noreply.github.com>
2024-12-18 22:37:16 +0530

    initial commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function createDatabase() {
-    mongoose.connect('mongodb://localhost:27017/show')
+    mongoose.connect('mongodb://localhost:27017/show')        //where show is database name
     .then(()=>{
         console.log('Connected to MongoDB');
     }).catch(()=>{
